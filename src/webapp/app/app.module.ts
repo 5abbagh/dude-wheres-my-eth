1557140e92389fae7e8e6cda80785e3c0d3bf21a
lkrausse <l.krausse@weptun.de>
2021-06-02 10:42:41 +0200

    Improve structure

--- src/webapp/app/app.module.ts
+++ src/webapp/app/app.module.ts
@@ -9,9 +9,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { AddRuleComponent } from './components/add-rule/add-rule.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, AddRuleComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
